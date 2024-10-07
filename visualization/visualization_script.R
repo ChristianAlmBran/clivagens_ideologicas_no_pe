1a2b4fb0b025472b0b81311e56b869aca3fdc7a6
Christian de Almeida Brandão <111160878+ChristianAlmBran@users.noreply.github.com>
2024-10-07 15:01:51 -0300

    Update visualization_script.R

--- visualization/visualization_script.R
+++ visualization/visualization_script.R
@@ -1,14 +1,14 @@
 pacman::p_load(tidyverse)
 
 # Loading csv files
-correct_final_vote_yes <- read.csv("correct_no_multicollinearity.csv")
-correct_final_vote_no <- read.csv("correct_no_mean_no_finalvote.csv")
-correct_yes_mean_final_vote_no <- read.csv("correct_yes_mean_no_finalvote.csv")
-correct_yes_mean_final_vote_yes <- read.csv("correct_yes_mean_yes_finalvote.csv")
+final_vote_yes <- read.csv("final_vote_yes.csv")
+final_vote_no <- read.csv("final_vote_no.csv")
+final_vote_no_mean <- read.csv("final_vote_no_mean.csv")
+final_vote_yes_mean <- read.csv("final_vote_yes_mean.csv")
 
 
 # Create the plot for final_vote_yes
-ggplot(correct_final_vote_yes, aes(x = parliament_session, y = predicted_prob, color = environment)) +
+ggplot(final_vote_yes, aes(x = parliament_session, y = predicted_prob, color = environment)) +
   geom_point(position = position_dodge(width = 0.5)) +
   geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2, position = position_dodge(width = 0.5)) +
   facet_wrap(~Indepentent_vari) +
@@ -25,11 +25,11 @@ ggplot(correct_final_vote_yes, aes(x = parliament_session, y = predicted_prob, c
     plot.background = element_rect(fill = "white", color = NA)
   )
 
-ggsave("correct_final_vote_yes.png", width = 28, height = 20, units = "cm", dpi = 300)
+ggsave("final_vote_yes.png", width = 28, height = 20, units = "cm", dpi = 300)
 
 
 # Create the plot for final_vote_no
-ggplot(correct_final_vote_no, aes(x = parliament_session, y = predicted_prob, color = environment)) +
+ggplot(final_vote_no, aes(x = parliament_session, y = predicted_prob, color = environment)) +
   geom_point(position = position_dodge(width = 0.5)) +
   geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2, position = position_dodge(width = 0.5)) +
   facet_wrap(~Indepentent_vari) +
@@ -46,11 +46,11 @@ ggplot(correct_final_vote_no, aes(x = parliament_session, y = predicted_prob, co
     plot.background = element_rect(fill = "white", color = NA)
   )
 
-ggsave("correct_final_vote_no.png", width = 28, height = 20, units = "cm", dpi = 300)
+ggsave("final_vote_no.png", width = 28, height = 20, units = "cm", dpi = 300)
 
 
-# Create the plot for yes_mean_final_vote_yes
-ggplot(correct_yes_mean_final_vote_yes, aes(x = parliament_session, y = predicted_prob, color = environment)) +
+# Create the plot for final_vote_yes_mean
+ggplot(final_vote_yes_mean, aes(x = parliament_session, y = predicted_prob, color = environment)) +
   geom_point(position = position_dodge(width = 0.5)) +
   geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2, position = position_dodge(width = 0.5)) +
   facet_wrap(~Indepentent_vari) +
@@ -67,11 +67,11 @@ ggplot(correct_yes_mean_final_vote_yes, aes(x = parliament_session, y = predicte
     plot.background = element_rect(fill = "white", color = NA)
   )
 
-ggsave("correct_yes_mean_final_vote_yes.png", width = 28, height = 20, units = "cm", dpi = 300)
+ggsave("final_vote_yes_mean.png", width = 28, height = 20, units = "cm", dpi = 300)
 
 
-# Create the plot for yes_mean_final_vote_no
-ggplot(correct_yes_mean_final_vote_no, aes(x = parliament_session, y = predicted_prob, color = environment)) +
+# Create the plot for final_vote_no_mean
+ggplot(final_vote_no_mean, aes(x = parliament_session, y = predicted_prob, color = environment)) +
   geom_point(position = position_dodge(width = 0.5)) +
   geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2, position = position_dodge(width = 0.5)) +
   facet_wrap(~Indepentent_vari) +
@@ -88,4 +88,4 @@ ggplot(correct_yes_mean_final_vote_no, aes(x = parliament_session, y = predicted
     plot.background = element_rect(fill = "white", color = NA)
   )
 
-ggsave("correct_yes_mean_final_vote_no.png", width = 28, height = 20, units = "cm", dpi = 300)
+ggsave("final_vote_no_mean.png", width = 28, height = 20, units = "cm", dpi = 300)
